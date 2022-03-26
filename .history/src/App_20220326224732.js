c7e97d6e2ed3694012c75aaae55cb611fd2c92f4
abhi-sharma27 <abhishekpurika@gmail.com>
2022-03-26 22:49:09 +0530

    Testing Completed March 2022

--- .history/src/App_20220326224732.js
+++ .history/src/App_20220326224732.js
@@ -8,8 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload the page.
+          
         </p>
-        <p>Temporary published on gitPages for testing purpose.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
